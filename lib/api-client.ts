30665ea7b91641ed40a8d6176ec071369d6719e4
SAEED.PANAHI <panahi.projects@gmail.com>
2025-05-25 23:25:57 +0330

    Feat: add other HTTP standard methods to the api-client

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -25,6 +25,36 @@ class ApiClient {
   ): Promise<ApiResponse<T>> {
     return this.instance.get<T>(url, config);
   }
+  public async post<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<ApiResponse<T>> {
+    return this.instance.post<T>(url, data, config);
+  }
+
+  public async put<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<ApiResponse<T>> {
+    return this.instance.put<T>(url, data, config);
+  }
+
+  public async delete<T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<ApiResponse<T>> {
+    return this.instance.delete<T>(url, config);
+  }
+
+  public async patch<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<ApiResponse<T>> {
+    return this.instance.patch<T>(url, data, config);
+  }
 }
 
 // Default instance
