c79e94b460426d97cb3b2ad0cd6a6496ebd8940a
SAEED.PANAHI <panahi.projects@gmail.com>
2025-05-25 08:34:07 +0330

    modify logo: dark/light mode

--- components/layout/logo/logo.tsx
+++ components/layout/logo/logo.tsx
@@ -1,10 +1,23 @@
+"use client";
 import Link from "next/link";
 import styles from "./logo.module.scss";
+import Image from "next/image";
+import { useTheme } from "@/providers/ThemeProvider";
 
 const Logo = () => {
+  const { theme } = useTheme();
   return (
     <Link href="/" className={styles.logo}>
-      TaxDo.
+      <Image
+        src={
+          theme === "dark"
+            ? "/assets/images/logo-white.svg"
+            : "/assets/images/logo-black.svg"
+        }
+        width={98}
+        height={29}
+        alt="TaxDo"
+      />
     </Link>
   );
 };
